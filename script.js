5e0c3a358144c053bb3cee81b13e1874f2b517f0
nakamura-86 <crossthesea200786@gmail.com>
2026-01-25 09:36:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const input = document.getElementById("imageInput");
 
+// ===== 切り抜き領域 =====
+  const FRAME_WINDOW = {
+  x: 55,
+  y: 90,
+  width: 404,
+  height: 153
+};
+
 // ===== 固定背景画像 =====
 const background = new Image();
 background.src = "image/event_news_bg.png";
@@ -50,13 +58,6 @@ input.addEventListener("change", (e) => {
 });
 
 // 描画
-
-  const FRAME_WINDOW = {
-  x: 55,
-  y: 90,
-  width: 404,
-  height: 153
-};
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
