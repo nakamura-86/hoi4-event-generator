3572b1e9937f4237b32807e1aa2dc4ba95be045e
nakamura-86 <crossthesea200786@gmail.com>
2026-01-25 09:49:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -84,18 +84,7 @@ function draw() {
   // ===== フレームを最前面 =====
   ctx.drawImage(background, 0, 0);
 
-  // ===== デバッグ用：赤枠を最前面に =====
-  ctx.save();
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 2;
-  ctx.strokeRect(
-    FRAME_WINDOW.x,
-    FRAME_WINDOW.y,
-    FRAME_WINDOW.width,
-    FRAME_WINDOW.height
-  );
-  ctx.restore();
-}
+
 
 
 
