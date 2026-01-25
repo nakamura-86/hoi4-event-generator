a01eefca41a3f1600ea79f9f160a082045ebc34e
nakamura-86 <crossthesea200786@gmail.com>
2026-01-25 10:21:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const texts = {
     x: 250,
     y: 250,
     font: "bold 28px serif",
+    ctx.textAlign = "center";
     color: "black"
   },
   description: {
@@ -17,6 +18,7 @@ const texts = {
     y: 290,
     width: 100,
     font: "16px serif",
+    ctx.textAlign = "center";
     color: "black"
   },
   option: {
@@ -24,6 +26,7 @@ const texts = {
     x: 250,
     y: 515,
     font: "16px serif",
+    ctx.textAlign = "center";
     color: "white"
   }
 };
